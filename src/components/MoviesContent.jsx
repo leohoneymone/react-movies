237e8266a26fdfd32666eacae6999c1cdb565500
Leo <leogc337@gmail.com>
2025-10-28 22:20:35 +0300

    Created template for movies content

--- src/components/MoviesContent.jsx
+++ src/components/MoviesContent.jsx
@@ -1,6 +1,11 @@
 export default function MoviesContent() {
 
     return <div className="content">
-        Content will be here
+        <div className="content-block">
+            <h2>Placeholder for search section</h2>
+        </div>
+        <div className="content-block">
+            <h2>Placeholder for display section</h2>
+        </div>
     </div>
 }
