4299b25d882eb11bd6a46392a9442702462966dc
Leo <leogc337@gmail.com>
2025-10-30 19:42:53 +0300

    Created placeholders for movies list

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,6 +3,31 @@ import { moviesContext } from "../utils/context";
 
 const APIKEY = process.env.REACT_APP_APIKEY;
 
+
+// Начальный плейсхолдер 
+function InitPlaceholder() {
+    return <div className="loading-and-placeholder-block">
+        <span className="placeholder-icon">🎬</span>
+        <p>There will be movies you'll find</p>
+    </div>
+}
+
+// Плейсхолдер для загрузки контента
+function LoadingPlaceholder() {
+    return <div className="loading-and-placeholder-block">
+        <span className="placeholder-icon loading-animated">💿</span>
+        <p>Loading...</p>
+    </div>
+}
+
+// Плейсхолдер для пустого вывода фильмов
+function NotFoundPlaceholder() {
+    return <div className="loading-and-placeholder-block">
+        <span className="placeholder-icon">🗿</span>
+        <p>Oops... Nothing found</p>
+    </div>
+}
+
 export default function MovieList() {
     // Состояния 
     const {data, setMovieData} = useContext(moviesContext);
@@ -23,9 +48,6 @@ export default function MovieList() {
     }
 
     return <div className="content-block movie-list-block">
-         <div className="loading-and-placeholder-block">
-            <span className="placeholder-icon">🎬</span>
-            <p>There will be movies you'll find</p>
-         </div>
+        
     </div>
 }
