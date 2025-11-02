e5617e8a1c71b9741991ee43f7e26154475fc417
Leo <leogc337@gmail.com>
2025-11-02 11:38:17 +0300

    Tested detailed movie info request

--- src/pages/MovieInfoPage.jsx
+++ src/pages/MovieInfoPage.jsx
@@ -1,8 +1,29 @@
 import { useParams } from "react-router-dom"
+import { useEffect } from "react";
+
+import { omdbApiRequest } from "../utils/api";
 
 export default function MovieInfoPage() {
     // Чтение imdbId из URL
     const movieId = useParams().id;
+
+    useEffect(() => {
+        if(!movieId){
+            return;
+        }
+
+        getMovieData(movieId);
+    }, []);
+
+    // Запрос полной информации о фильме
+    const getMovieData = async (movieId) => {
+        const apiParameters = `&i=${movieId}&plot=full`;
+        const data = await omdbApiRequest(apiParameters)
+            .catch(e => {
+                console.error(`Fetch API error: ${e} (${e.message})`);
+            });
+        console.log(data);
+    }
 
     return <div className="content-block full-sized-block ">
         <p>Searching for moive with ID: {movieId}</p>
