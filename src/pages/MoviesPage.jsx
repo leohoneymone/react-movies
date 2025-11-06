d5c4f957fb993f99334fb6c98721aa2b7a82feb2
Leo <leogc337@gmail.com>
2025-11-06 21:28:40 +0300

    Minimized movies page

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,13 +1,7 @@
 import MovieSelect from "../components/MovieSelect";
 import MovieList from "../components/MovieList";
-import Modal from "../components/layout/Modal";
-
-import { useContext } from "react";
-import { moviesContext } from "../utils/context";
 
 export default function MoviesPage() {
-    const {modalMessage} = useContext(moviesContext);
-
     return <>
         <MovieSelect />
         <MovieList />
