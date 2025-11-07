aed134a19925b341eac8f6114b6fbc862f958029
Leo <leogc337@gmail.com>
2025-11-07 20:21:05 +0300

    Added comments to context file

--- src/utils/context.js
+++ src/utils/context.js
@@ -44,62 +44,59 @@ export default function Context ({children}){
     }, [movieWatchList]);
 
     // Добавление функций для работы с состояниями в редьюсер
+    // Выгрузка списка фильмов
     appValue.setMovieData = data => {
         dispatcher({type: "SET_MOVIE_DATA", payload: data});
     }
-
     appValue.setLoading = (value) => {
         dispatcher({type: "SET_LOADING", payload: value});
     }
-
     appValue.setRequested = (value) => {
         dispatcher({type: "SET_REQUESTED", payload: value});
     }
-    
     appValue.setSearch = data => {
         dispatcher({type: "SET_SEARCH_DATA", payload: data});
     }
-
     appValue.setTotalResults = data => {
         dispatcher({type: 'SET_TOTAL_RESULTS', payload: data});
     }
-
     appValue.setTotalPages = data => {
         dispatcher({type: 'SET_TOTAL_PAGES', payload: data});
     }
 
+    // Пагинация
     appValue.selectPage = data =>{
         dispatcher({type: 'SELECT_PAGE', payload: data});
     }
 
+    // Очистка данных
     appValue.clearData = () => {
         dispatcher({type: 'CLEAR_DATA'});
     }
 
+    // Полная информация о фильме
     appValue.setMovieDetailedInfo = data => {
         dispatcher({type: "SET_MOVIE_DETAILED_INFO", payload: data});
     }
 
+    // Работа с модальным окном
     appValue.setModalMessage = text => {
         dispatcher({type: "SET_MODAL_MESSAGE", payload: text});
     }
 
+    // Работа со списком данных для просмотра
     appValue.addMovieToWatchList = data => {
         dispatcher({type: "ADD_TO_WATCH_LIST", payload: data});
     }
-
     appValue.removeFromWatchList = id => {
         dispatcher({type: "REMOVE_FROM_WATCH_LIST", payload: id});
     }
-
     appValue.setWatched = id => {
         dispatcher({type: "SET_WATCHED", payload: id});
     }
-
     appValue.unsetWatched = id => {
         dispatcher({type: "UNSET_WATCHED", payload: id});
     }
-
     appValue.calculateRuntime = () => {
         dispatcher({type: 'CALCULATE_RUNTIME'});
     }
